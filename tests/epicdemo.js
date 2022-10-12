4006f8867bd400ffef716a1b8a3e3429cfb5d427
clariti23 <g.ferowich@gmail.com>
2022-10-11 19:04:16 -0500

    deployed contract

--- tests/epicdemo.js
+++ tests/epicdemo.js
@@ -1,13 +1,53 @@
 const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-describe("epicdemo", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Epicdemo;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+const main = async () => {
+  console.log("Starting test...");
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.Epicdemo;
+  
+  // this is where the test account is instantiated
+  const baseAccount = anchor.web3.Keypair.generate();
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId, 
+    },
+    signers: [baseAccount],
   });
-});
+  
+  
+  console.log("Your transaction signature", tx);
+  
+  //initite the gif counter
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('Gif count', account.totalGifs.toString());
+
+  await program.rpc.addGif("https://media.giphy.com/media/3oEhn51EPgFiSQw8GQ/giphy.gif",{
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed. 
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());   
+
+  console.log('Gif list', account.gifList)
+}
+
+//runMain actually is the container of the test function
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1)
+  }
+};
+
+runMain();
